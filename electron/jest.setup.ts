8821c61b97f5bfc0cee30397ea659ceaa2e3b024
gan_xingba <1063010784@qq.com>
2026-07-14 16:56:31 +0800

    Use writable Electron Jest userData path

--- electron/jest.setup.ts
+++ electron/jest.setup.ts
@@ -1,10 +1,21 @@
 // jest.setup.ts
 import { jest } from '@jest/globals';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+const mockUserDataPath = path.join(os.tmpdir(), 'image-management-jest-userData');
+fs.mkdirSync(mockUserDataPath, { recursive: true });
 
 // Mock electron
 jest.mock('electron', () => ({
   app: {
-    getPath: jest.fn(() => '/mock/path'),
+    getPath: jest.fn((name?: string) => {
+      if (name === 'userData') {
+        return mockUserDataPath;
+      }
+      return mockUserDataPath;
+    }),
     isPackaged: false, // Ensure this is set to a boolean value
     on: jest.fn(),
     whenReady: jest.fn(() => Promise.resolve()),
